2bd847a12d5f7fa881b12f6bd61d305f135f849e
Francisco Monteiro <francisco.r.monteiro@gmail.com>
2011-10-02 14:40:13 +0100

    adding multi language support

---
+++ trunk/OGen-Kick/db/Kick-scripts-Postgresql/fnc_DIC_TextLanguage_Record_open_byLanguage_short.sql
@@ -0,0 +1,32 @@
+﻿CREATE OR REPLACE FUNCTION "fnc_DIC_TextLanguage_Record_open_byLanguage_short"("IDLanguage_search_" integer)
+	RETURNS SETOF "v0_DIC_TextLanguage__onlyKeys" AS
+$BODY$
+	DECLARE
+		_Output "v0_DIC_TextLanguage__onlyKeys";
+	BEGIN
+		FOR _Output IN
+
+			select
+				t3."IFText", 
+				t3."IFLanguage"
+			from "DIC_Language" as t1
+			inner join "DIC_Text" as t2 on (
+				t2."IDText" = t1."TX_Name"
+			)
+			inner join "DIC_TextLanguage" as t3 on (
+				t3."IFText" = t2."IDText"
+			)
+			where t3."IFLanguage" = "IDLanguage_search_"
+
+		LOOP
+			RETURN NEXT _Output;
+		END LOOP;
+
+		RETURN;
+	END;
+$BODY$
+	LANGUAGE plpgsql STABLE
+	COST 100
+	ROWS 1000;
+ALTER FUNCTION "fnc_DIC_TextLanguage_Record_open_byLanguage_short"(integer)
+	OWNER TO postgres;
---
+++ trunk/OGen-Kick/db/Kick-scripts-SQLServer/fnc_DIC_TextLanguage_Record_open_byLanguage_short.sql
@@ -0,0 +1,20 @@
+﻿alter FUNCTION [dbo].[fnc_DIC_TextLanguage_Record_open_byLanguage_short](
+	@IDLanguage_search_ int
+)
+RETURNS TABLE
+AS
+RETURN
+
+			select
+				t3."IFText", 
+				t3."IFLanguage"
+			from "DIC_Language" as t1
+			inner join "DIC_Text" as t2 on (
+				t2."IDText" = t1."TX_Name"
+			)
+			inner join "DIC_TextLanguage" as t3 on (
+				t3."IFText" = t2."IDText"
+			)
+			where t3."IFLanguage" = @IDLanguage_search_
+
+GO
