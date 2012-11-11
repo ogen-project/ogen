440238fa9316e2fadea840ed521847196602edb0
Francisco Monteiro <francisco.r.monteiro@gmail.com>
2012-11-11 12:40:19 +0000

    .

--- trunk/OGen/OGen_datalayersupport/DBServerTypes.cs
+++ trunk/OGen/OGen_datalayersupport/DBServerTypes.cs
@@ -16,7 +16,7 @@ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLI
 namespace OGen.Libraries.DataLayer {
 	using System;
 
-	public enum DBServerTypes {
+	public enum DBServerTypes : int {
 #if PostgreSQL
 		PostgreSQL = 0, 
 #endif
